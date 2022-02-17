c693c15e7371c8578ca18d9fa66d105faba2f8e0
Rkl1993 <rupali.londhe@successive.tech>
2022-02-17 16:52:53 +0530

    feature/task2: task completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule,ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import {AppRoutingModule}from './app-routing.module';
 import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
@@ -14,7 +14,9 @@ import { TemplateFormComponent } from './components/template-form/template-form.
   ],
   imports: [
     BrowserModule, 
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
